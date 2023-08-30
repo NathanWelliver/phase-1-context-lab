72672c608ccf98dbecf6804eb9e291c5fb219946
Nathan Welliver <nwelliver.564@gmail.com>
2023-08-30 16:48:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+
+    const timeInHour = timeInEvent.hour;
+    const timeOutHour = timeOutEvent.hour;
+
+    return (timeOutHour - timeInHour) / 100; // Convert to hours
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const payRate = this.payPerHour;
+
+    return hoursWorked * payRate;
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce(function (totalPayroll, employee) {
+        return totalPayroll + allWagesFor.call(employee);
+    }, 0);
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +84,15 @@ const allWagesFor = function () {
     return payable
 }
 
+// Export the functions
+module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll
+};
